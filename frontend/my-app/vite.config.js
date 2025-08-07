1c66b832f54fba0ba75142e3e2e80b19832b7282
taisan10 <tarunbansalsgnr@gmail.com>
2025-08-08 03:14:12 +0530

    Initial commit

--- frontend/my-app/vite.config.js
+++ frontend/my-app/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+   base: "/",
   plugins: [tailwindcss(), react()],
 });
